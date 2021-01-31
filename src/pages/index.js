63005a059b9b312dbf1599bfa93397a57a7ce60d
Facundo Prieto <facundo.prieto321@gmail.com>
2021-01-30 22:42:56 -0300

    Index hardcode

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,33 @@
 import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
-import { Hero } from '../components/Hero'
+  useColorMode,
+  useDisclosure
+} from "@chakra-ui/react"
+import { AddSubject } from '../components/AddSubject'
+import { Card } from "../components/Card"
+import { ClassCard } from "../components/ClassCard"
 import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+import { Content } from '../components/Content'
+import { Header } from '../components/Header'
+import { ModalAddClass } from '../components/ModalAddClass'
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code>.
-      </Text>
+const Index = () => {
+  const { isOpen: isOpenModal, onOpen: onOpenModal, onClose: onCloseModal } = useDisclosure()
+  const { isOpen: isOpenAddSubject, onToggle: onToggleAddSubject } = useDisclosure()
 
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
-
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
+  return (
+    <Container minH="100vh">
+      <Header />
+      <Content>
+        <Card code="(75,05)" subject="Analisis" openModal={onOpenModal}>
+          <ClassCard />
+          <ClassCard />
+          <ClassCard />
+        </Card>
+        <AddSubject isOpen={isOpenAddSubject} onToggle={onToggleAddSubject} />
+      </Content>
+      <ModalAddClass isOpen={isOpenModal} onOpen={onOpenModal} onClose={onCloseModal} />
+    </Container >
+  )
+}
 
 export default Index
