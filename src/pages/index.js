670aac76ea7427b830b73d4b9dab2e832b649374
Facundo Prieto <facundo.prieto321@gmail.com>
2021-03-24 19:40:20 -0300

    main page finish version

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,75 @@
 import {
-  useColorMode,
   useDisclosure
 } from "@chakra-ui/react"
+import { useContext, useEffect, useState } from "react"
+import { DragDropContext } from "react-beautiful-dnd"
 import { AddSubject } from '../components/AddSubject'
 import { Card } from "../components/Card"
-import { ClassCard } from "../components/ClassCard"
-import { Container } from '../components/Container'
 import { Content } from '../components/Content'
-import { Header } from '../components/Header'
 import { ModalAddClass } from '../components/ModalAddClass'
+import { DispatchContext, StateContext } from '../Context'
+
+
+function onDragEnd(result, state, dispatch) {
+  const { destination, source, draggableId } = result;
+
+  if (!destination) {
+    return;
+  }
+
+  if (
+    destination.droppableId === source.droppableId &&
+    destination.index === source.index
+  ) {
+    return;
+  }
+
+  const subject = state.subjects[source.droppableId]
+  const newLessonIds = Array.from(subject.lessonIds)
+  newLessonIds.splice(source.index, 1)
+  newLessonIds.splice(destination.index, 0, draggableId)
+
+  const newSubject = {
+    ...subject,
+    lessonIds: newLessonIds
+  }
+
+  const newState = {
+    ...state,
+    subjects: {
+      ...state.subjects,
+      [newSubject.id]: newSubject,
+    }
+  }
+
+  dispatch({ type: 'reorderOnDragEnd', state: newState })
+
+}
 
 const Index = () => {
   const { isOpen: isOpenModal, onOpen: onOpenModal, onClose: onCloseModal } = useDisclosure()
   const { isOpen: isOpenAddSubject, onToggle: onToggleAddSubject } = useDisclosure()
+  const [subjectIdModal, setSubjectIdModal] = useState("")
 
-  return (
-    <Container minH="100vh">
-      <Header />
-      <Content>
-        <Card code="(75,05)" subject="Analisis" openModal={onOpenModal}>
-          <ClassCard />
-          <ClassCard />
-          <ClassCard />
-        </Card>
-        <AddSubject isOpen={isOpenAddSubject} onToggle={onToggleAddSubject} />
-      </Content>
-      <ModalAddClass isOpen={isOpenModal} onOpen={onOpenModal} onClose={onCloseModal} />
-    </Container >
+  const state = useContext(StateContext)
+  const dispatch = useContext(DispatchContext);
+
+  return (<>
+    <Content>
+      <DragDropContext onDragEnd={(result) => (onDragEnd(result, state, dispatch))}>
+        {state.subjectOrder.map(subjectId => {
+          const subject = state.subjects[subjectId]
+          const lessons = subject.lessonIds.map(lessonId => state.lesson[lessonId])
+
+          return (
+            <Card key={subject.id} subject={subject} lessons={lessons} openModal={() => { setSubjectIdModal(subjectId); onOpenModal() }} />
+          )
+        })}
+      </DragDropContext>
+      <AddSubject isOpen={isOpenAddSubject} onToggle={onToggleAddSubject} />
+    </Content>
+    <ModalAddClass isOpen={isOpenModal} onOpen={onOpenModal} subjectId={subjectIdModal} onClose={() => { onCloseModal(); setSubjectIdModal("") }} />
+  </>
   )
 }
 
