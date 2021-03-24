10d287382af648de0cdc9a9d79f74ebbbb8d12fe
Facundo Prieto <facundo.prieto321@gmail.com>
2021-03-24 19:41:25 -0300

    draggable components

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,26 @@ import {
     Link,
     useColorMode,
 } from "@chakra-ui/react"
+import { Droppable } from "react-beautiful-dnd"
+import { ClassCard } from "../components/ClassCard"
 
-export const Card = (props) => {
+
+export const Card = ({ subject, lessons, openModal }) => {
     const { colorMode } = useColorMode()
     const bgColor = { light: 'gray.100', dark: 'gray.600' }
 
     return (
-        <Flex flexDir="column" bg={bgColor[colorMode]} borderRadius="md" p={2} m={1} w={300} minW={300} {...props}>
-            <Heading size="sm" mb={2} ml={1} mt={1}>{props.code} {props.subject}</Heading>
-            {props.children}
-            <Link mt={2} ml={1} onClick={props.openModal} fontSize="sm">Add Catedra...</Link>
+        <Flex flexDir="column" bg={bgColor[colorMode]} borderRadius="md" p={2} m={1} w={300} minW={300} >
+            <Heading size="sm" mb={2} ml={1} mt={1}>{subject.code} {subject.name}</Heading>
+            <Droppable droppableId={subject.id}>
+                {provided => (
+                    <Flex flexDir="column" ref={provided.innerRef} {...provided.droppableProps}>
+                        {lessons.map((lesson, index) => <ClassCard key={lesson.id} lesson={lesson} index={index} />)}
+                        {provided.placeholder}
+                    </Flex>
+                )}
+            </Droppable>
+            <Link mt={2} ml={1} onClick={openModal} fontSize="sm">Add Catedra...</Link>
         </Flex>
     )
 }
--- src/components/ClassCard.js
+++ src/components/ClassCard.js
@@ -9,27 +9,46 @@ import {
     AccordionPanel,
     AccordionIcon,
 } from "@chakra-ui/react"
+import { Draggable } from "react-beautiful-dnd"
 
-export const ClassCard = (props) => {
+export const ClassCard = ({ lesson, index }) => {
     const { colorMode } = useColorMode()
     const bgColor = { light: 'gray.50', dark: 'gray.500' }
 
     return (
-        <Accordion /* defaultIndex={[0]} */ allowMultiple>
-            <AccordionItem bg={bgColor[colorMode]} border={0} borderRadius="md" mt={2} >
-                <AccordionButton>
-                    <Box flex="1" textAlign="left">
-                        1. Curso - Catedra
-                        </Box>
-                    <AccordionIcon />
-                </AccordionButton>
-                <AccordionPanel pb={4}>
-                    <Flex flexDir="column">
-                        <Text>xx:xx - xx:xx, Dia</Text>
-                        <Text>xx:xx - xx:xx, Dia</Text>
-                    </Flex>
-                </AccordionPanel>
-            </AccordionItem>
-        </Accordion>
+        <Draggable draggableId={lesson.id} index={index}>
+            {provided => (
+                <div
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef} >
+                    <Accordion /* defaultIndex={[0]} */ allowMultiple>
+                        <AccordionItem bg={bgColor[colorMode]} border={0} borderRadius="md" mt={2} >
+                            <Flex alignItems="center" py={2} px={3}>
+                                <Box flex="1" textAlign="left">
+                                    {index + 1}. {lesson.lessonCode} - {lesson.teacher}
+                                </Box>
+                                <Box>
+                                    <AccordionButton borderRadius="md">
+                                        <AccordionIcon />
+                                    </AccordionButton>
+                                </Box>
+                            </Flex>
+                            <AccordionPanel pb={4}>
+                                <Flex flexDir="column">
+                                    {lesson.schedules.map((schedule, index) => {
+                                        return (
+                                            <Text key={schedule.id}>{schedule.since} - {schedule.until}, {schedule.day.charAt(0).toUpperCase() + schedule.day.slice(1)}</Text>
+                                        )
+                                    })}
+                                </Flex>
+                            </AccordionPanel>
+                        </AccordionItem>
+                    </Accordion>
+                </div>
+            )}
+        </Draggable >
     )
 }
+
+
