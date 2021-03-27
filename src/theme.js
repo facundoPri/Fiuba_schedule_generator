0e8b3f6daff241178a24a0f3c9c96da8d1b009ae
Facundo Prieto <facundo.prieto321@gmail.com>
2021-03-27 18:04:37 -0300

    Add robot font

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,11 @@
 import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
+  fonts: {
+    body: "roboto, sans-serif",
+    heading: "roboto, sans-serif",
+    mono: "roboto, sans-serif",
+  },
 })
 
 export default theme
