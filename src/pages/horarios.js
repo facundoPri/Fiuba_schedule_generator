a3caf1062eaf3b0c6602bf59fcc2eb9b7cd45192
Facundo Prieto <facundo.prieto321@gmail.com>
2023-07-24 12:02:16 -0300

    fix error on horarios page when no clases added

--- src/pages/horarios.js
+++ src/pages/horarios.js
@@ -1,118 +1,97 @@
-import { IconButton } from '@chakra-ui/button'
-import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons'
-import { Flex, Text } from '@chakra-ui/layout'
-import { useEffect, useState } from 'react'
-import { Calendar } from '../components/Calendar'
-import { useDataStore } from '../Context'
+import { IconButton } from "@chakra-ui/button";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import { Flex, Text } from "@chakra-ui/layout";
+import { useEffect, useState } from "react";
+import { Calendar } from "../components/Calendar";
+import { useDataStore } from "../Context";
 
-import { schedulesGenerator, toMinutes } from '../utils/schedulesGenerator_v2'
+import { schedulesGenerator, toMinutes } from "../utils/schedulesGenerator_v2";
 
 const hours = [
-  5,
-  6,
-  7,
-  8,
-  9,
-  10,
-  11,
-  12,
-  13,
-  14,
-  15,
-  16,
-  17,
-  18,
-  19,
-  20,
-  22,
-  23
-]
+  5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 23,
+];
 const days = [
-  'lunes',
-  'martes',
-  'miercoles',
-  'jueves',
-  'viernes' /* , 'sabado', 'domingo' */
-]
+  "lunes",
+  "martes",
+  "miercoles",
+  "jueves",
+  "viernes" /* , 'sabado', 'domingo' */,
+];
 
 const Horarios = () => {
-  const [schedulesDays, setSchedulesDays] = useState([])
-  const [optionIndex, setOptionIndex] = useState(0)
-  const { options, lessons, subjects, subjectsOrder, addScheduleOptions } = useDataStore(state => ({
-    options: state.options,
-    lessons: state.lessons,
-    subjects: state.subjects,
-    subjectsOrder: state.subjectsOrder,
-    addScheduleOptions: state.addScheduleOptions,
-  }))
+  const [schedulesDays, setSchedulesDays] = useState([]);
+  const [optionIndex, setOptionIndex] = useState(0);
+  const { options, lessons, subjects, subjectsOrder, addScheduleOptions } =
+    useDataStore((state) => ({
+      options: state.options,
+      lessons: state.lessons,
+      subjects: state.subjects,
+      subjectsOrder: state.subjectsOrder,
+      addScheduleOptions: state.addScheduleOptions,
+    }));
 
   useEffect(() => {
     if (options.length > 0) {
-      setSchedulesDays(generateSchedulesDays(options))
+      setSchedulesDays(generateSchedulesDays(options));
     } else {
       let scheduleOptions = schedulesGenerator({
         subjects,
         lessons,
-        subjectsOrder
-      })
-      console.log({scheduleOptions})
-      addScheduleOptions(scheduleOptions)
-      setSchedulesDays(generateSchedulesDays(scheduleOptions))
+        subjectsOrder,
+      });
+      addScheduleOptions(scheduleOptions);
+      setSchedulesDays(generateSchedulesDays(scheduleOptions));
     }
-  }, [])
+  }, []);
 
-  const handleOptionChange = value => {
-    console.log(value)
+  const handleOptionChange = (value) => {
     if (0 <= value && value <= schedulesDays.length - 1) {
-      console.log(schedulesDays[value])
-      console.log('TExt')
-      setOptionIndex(value)
+      setOptionIndex(value);
     }
-  }
+  };
 
-  const generateSchedulesDays = options => {
-    let optionsDays = []
-    console.log(options)
-    options.forEach(option => {
-      let days = {}
-      option.forEach(lesson => {
-        lessons[lesson].schedules.forEach(schedule => {
-          let { day, since, until } = schedule
-          day = day.toLowerCase()
-          until = toMinutes(until) / 60
-          since = toMinutes(since) / 60
-          let duration = until - since
-          let hour = { since, duration, lesson }
-          days[day] ? days[day].push(hour) : (days[day] = [hour])
-        })
-      })
-      optionsDays.push(days)
-    })
-    return optionsDays
-  }
+  const generateSchedulesDays = (options) => {
+    let optionsDays = [];
+    options.forEach((option) => {
+      let days = {};
+      option.forEach((lesson) => {
+        lessons[lesson].schedules.forEach((schedule) => {
+          let { day, since, until } = schedule;
+          day = day.toLowerCase();
+          until = toMinutes(until) / 60;
+          since = toMinutes(since) / 60;
+          let duration = until - since;
+          let hour = { since, duration, lesson };
+          days[day] ? days[day].push(hour) : (days[day] = [hour]);
+        });
+      });
+      optionsDays.push(days);
+    });
+    return optionsDays;
+  };
 
   return (
-    <Flex flexDir='column' overflowY='auto'>
-      <Flex justify='center' alignItems='baseline' m={4}>
+    <Flex flexDir="column" overflowY="auto">
+      <Flex justify="center" alignItems="baseline" m={4}>
         <IconButton
-          variant='unstyled'
-          aria-label='Previous Option'
-          fontSize='25px'
+          variant="unstyled"
+          aria-label="Previous Option"
+          fontSize="25px"
           icon={<ChevronLeftIcon />}
           onClick={() => {
-            handleOptionChange(optionIndex - 1)
+            handleOptionChange(optionIndex - 1);
           }}
         />
-        <Text fontSize='xl' mx={2}>
+        <Text fontSize="xl" mx={2}>
           Opction {optionIndex + 1}
         </Text>
         <IconButton
-          variant='unstyled'
-          aria-label='Next Option'
-          fontSize='25px'
+          variant="unstyled"
+          aria-label="Next Option"
+          fontSize="25px"
           icon={<ChevronRightIcon />}
           onClick={() => {
-            handleOptionChange(optionIndex + 1)
+            handleOptionChange(optionIndex + 1);
           }}
         />
       </Flex>
@@ -122,7 +101,7 @@ const Horarios = () => {
         schedules={schedulesDays[optionIndex]}
       />
     </Flex>
-  )
-}
+  );
+};
 
-export default Horarios
+export default Horarios;
--- src/utils/schedulesGenerator_v2.js
+++ src/utils/schedulesGenerator_v2.js
@@ -15,100 +15,100 @@
 // }
 
 // receive a time string (hh:mm) and return it value in minutes
-export function toMinutes (time) {
-  let hms = time.split(':')
-  let minutes = +hms[0] * 60 + +hms[1]
-  return minutes
+export function toMinutes(time) {
+  let hms = time.split(":");
+  let minutes = +hms[0] * 60 + +hms[1];
+  return minutes;
 }
 
 // receive two schedules arrays and return if they overlap or not
-function checkScheduleCompability (schedule1, schedule2) {
-  let days = {}
-  let compatible = true
+function checkScheduleCompability(schedule1, schedule2) {
+  let days = {};
+  let compatible = true;
   schedule1
     .concat(schedule2)
-    .map(schedule =>
+    .map((schedule) =>
       (days[schedule.day] = days[schedule.day] || []).push(schedule)
-    )
+    );
   for (let day in days) {
     days[day].sort((a, b) => {
-      let aMin = toMinutes(a.since)
-      let bMin = toMinutes(b.since)
+      let aMin = toMinutes(a.since);
+      let bMin = toMinutes(b.since);
       if (aMin > bMin) {
-        return 1
+        return 1;
       }
       if (aMin < bMin) {
-        return -1
+        return -1;
       }
-      return 0
-    })
+      return 0;
+    });
     for (let i = 0; i < days[day].length; i++) {
-      if (i == 0) continue
-      let actual = days[day][i]
-      let anterior = days[day][i - 1]
+      if (i == 0) continue;
+      let actual = days[day][i];
+      let anterior = days[day][i - 1];
       if (toMinutes(anterior.until) > toMinutes(actual.since)) {
-        compatible = false
-        return compatible
+        compatible = false;
+        return compatible;
       }
     }
   }
-  return compatible
+  return compatible;
 }
 
 // receive a lesson id and return it subject
-function find_subject (lessonId, data) {
-  let subjectId = data.subjectOrder.find(subject =>
+function find_subject(lessonId, data) {
+  let subjectId = data.subjectOrder.find((subject) =>
     data.subjects[subject].lessonIds.includes(lessonId)
-  )
-  return subjectId
+  );
+  return subjectId;
 }
 
 // Receive an objects with all the subjects and it lessons, an array with the order of the subjects and a object with the lessons info
-function generateCompabilityList (subjectsObject, subjectOrder, lessons) {
-  let combination = {}
+function generateCompabilityList(subjectsObject, subjectOrder, lessons) {
+  let combination = {};
   subjectOrder.forEach((subjectId1, index1) => {
     subjectOrder.forEach((subjectId2, index2) => {
       if (index1 >= index2) {
-        return
+        return;
       }
-      subjectsObject[subjectId1].forEach(lesson1 => {
-        subjectsObject[subjectId2].forEach(lesson2 => {
-          let lessonsValue1 = lessons[lesson1]
-          let lessonsValue2 = lessons[lesson2]
+      subjectsObject[subjectId1].forEach((lesson1) => {
+        subjectsObject[subjectId2].forEach((lesson2) => {
+          let lessonsValue1 = lessons[lesson1];
+          let lessonsValue2 = lessons[lesson2];
           if (
             checkScheduleCompability(
               lessonsValue1.schedules,
               lessonsValue2.schedules
             )
           ) {
-            combination[lesson1] = combination[lesson1] || []
-            combination[lesson1].push(lesson2)
+            combination[lesson1] = combination[lesson1] || [];
+            combination[lesson1].push(lesson2);
 
-            combination[lesson2] = combination[lesson2] || []
-            combination[lesson2].push(lesson1)
+            combination[lesson2] = combination[lesson2] || [];
+            combination[lesson2].push(lesson1);
           }
-        })
-      })
-    })
-  })
-  return combination
+        });
+      });
+    });
+  });
+  return combination;
 }
 
 // Receive an array and return an array with the values that repeats
-function removeUniquesValues (array) {
-  let result = []
+function removeUniquesValues(array) {
+  let result = [];
   while (array.length > 0) {
-    let lastValue = array.pop()
+    let lastValue = array.pop();
     if (array.includes(lastValue) && !result.includes(lastValue)) {
-      result.push(lastValue)
+      result.push(lastValue);
     }
   }
-  return result
+  return result;
 }
 
 // Receive a lessonId and an object with all the lessons and it compabilities
 // TODO: Loot of room to improvement
-function create_combinations (
+function create_combinations(
   lessonId,
   lessonCompabilities,
   possibleCombination = [],
@@ -116,69 +116,67 @@ function create_combinations (
   combinations = []
 ) {
   if (combination.length == 0) {
-    possibleCombination = lessonCompabilities[lessonId] || []
+    possibleCombination = lessonCompabilities[lessonId] || [];
   } else {
     possibleCombination = removeUniquesValues(
       possibleCombination.concat(lessonCompabilities[lessonId])
-    )
+    );
   }
 
-  combination = combination.concat(lessonId)
+  combination = combination.concat(lessonId);
 
   // if (possibleCombination.length == 0) {
 
   //   combinations.push(combination)
   //   combination = []
   // }
-  possibleCombination.forEach(lesson => {
+  possibleCombination.forEach((lesson) => {
     lessonCompabilities[lesson] = lessonCompabilities[lesson].filter(
-      l => l != lessonId
-    )
+      (l) => l != lessonId
+    );
     return create_combinations(
       lesson,
       lessonCompabilities,
       possibleCombination,
       combination,
       combinations
-    )
-  })
+    );
+  });
 
-  combinations.push(combination)
-  combination = []
-  return combinations
+  combinations.push(combination);
+  combination = [];
+  return combinations;
 }
 
 // Receive an array and a value, return the score for the value
-function calculateScore (value, arr) {
-  len = arr.length
-  index = arr.indexOf(value)
-  return ((len - index) / len) * 10
+function calculateScore(value, arr) {
+  len = arr.length;
+  index = arr.indexOf(value);
+  return ((len - index) / len) * 10;
 }
 
 // function for the sorting
 // TODO: Improve sorting function
-function sortCombinations (combinationA, combinationB) {
-  return combinationB.length - combinationA.length
+function sortCombinations(combinationA, combinationB) {
+  return combinationB.length - combinationA.length;
 }
 
-export function schedulesGenerator ({ subjects, lessons, subjectsOrder }) {
-  console.log({ subjects, lessons, subjectsOrder })
-  let subjectsObject = {}
+export function schedulesGenerator({ subjects, lessons, subjectsOrder }) {
+  let subjectsObject = {};
   Object.entries(subjects).map(
-    value => (subjectsObject[value[0]] = value[1].lessonIds)
-  )
-  console.log({ subjectsObject })
+    (value) => (subjectsObject[value[0]] = value[1].lessonIds)
+  );
+  console.log({ subjectsObject });
 
   let lessonCompability = generateCompabilityList(
     subjectsObject,
     subjectsOrder,
     lessons
-  )
-  console.log({ lessonCompability })
-  let combinations = []
-  subjectsObject[subjectsOrder[0]].forEach(lesson => {
-    combinations.push(...create_combinations(lesson, lessonCompability))
-  })
+  );
+  let combinations = [];
+  subjectsObject[subjectsOrder[0]]?.forEach((lesson) => {
+    combinations.push(...create_combinations(lesson, lessonCompability));
+  });
 
-  return combinations.sort(sortCombinations)
+  return combinations.sort(sortCombinations);
 }
